d0a53d6a87df2e965fa4597d5c3ed8d4ce79f65b
Naadir Al Atilla <82509877+NavdPlay@users.noreply.github.com>
2022-10-15 18:27:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,70 @@
-function addList() {
-    var text = document.getElementById('name-input').value;
-    if (text == '' || text == undefined) {
-        document.getElementById('popup-container').style.display = 'block'; 
-        return text;
-    }
-    var node = document.createElement('LI');
-    node.classList.add('list-child');
-    var textnode = document.createTextNode(text);
-    node.appendChild(textnode);
-    document.getElementById('list').appendChild(node);
+function toggleWidth(x) {
+  if (x.id == 'line-0') {
+    x.style.width = 'calc(45% + 1px)';
+    x.setAttribute('id', 'line-1');
+  } else {
+    x.style.width = '0';
+    x.setAttribute('id', 'line-0');
+  }
 }
 
-$('#name-input').keyup(
-    function() {
-        document.getElementById('popup-container').style.display = 'none'; 
-    }
-);
+function loadTasks() {
+  $('.listBox').html('');
+  $('.notice').css({
+    'display': 'none',
+  });
 
-$(document).on('click', '.list-child', function() {
-    if ($(this).hasClass('checked')) {
-        $(this).removeClass('checked');
+  if (!localStorage.hasOwnProperty('tasks') || localStorage.tasks == '') {
+    $('.notice').css({
+      'display': 'block',
+    });
+    return false;
+  }
+  var textTasks = localStorage.tasks;
+  textTasks = textTasks.split('|').map(i => i.split('.'));
+  for (var i = 0; i < textTasks.length; i++) {
+    $('.listBox').append(`<li id="${textTasks[i][1]}" onclick="removeTask(this)">${textTasks[i][0]}<hr class="taskLine"></li>`);
+  }
+}
+
+$('.prompt').keyup(function() {
+  if (event.which == 13) {
+    var random = '';
+    for (var i = 0; i < 6; i++) {
+      var num = Math.floor(Math.random() * 16);
+      random += num;
+    }
+    if (localStorage.tasks == undefined || localStorage.tasks == '') {
+      localStorage.tasks = `${$('.prompt').val()}.${parseInt(random).toString(16)}`;
     } else {
-        $(this).addClass('checked');
+      localStorage.tasks += `|${$('.prompt').val()}.${parseInt(random).toString(16)}`;
     }
+    $('.listBox').html('');
+    loadTasks();
+    $('.prompt').val('');
+  }
 });
 
-$(document).on('click', '#add-btn', function() {
-    $('.fas').remove();
-    $('.list-child').append('<i class="fas fa-times"></i>')
-});
+function removeTask(x) {
+  var confirmation = confirm(`Delete task: "${x.innerHTML.split('<')[0]}"?`);
+  if (!confirmation) {
+    return true;
+  }
+  var textTasks = localStorage.tasks;
+  textTasks = textTasks.split('|').map(i => i.split('.'));
+  for (var i = 0; i < textTasks.length; i++) {
+    if (textTasks[i][1] == x.id) {
+      textTasks.splice(i, 1);
+    }
+  }
+  for (var i = 0; i < textTasks.length; i++) {
+    textTasks[i] = textTasks[i].join('.');
+  }
+  textTasks = textTasks.join('|');
 
-$(document).on('click', '.fas', function() {
-    $(this).parent().remove();
-    $(this).remove();
-});
+  localStorage.tasks = '';
+  localStorage.tasks += textTasks;
+  loadTasks();
+}
+
+loadTasks();
